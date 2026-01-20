d9a83a54e0274ab2cb9d999cd8cb7ece354e0cea
Joao Paulo Lordelo <joao.lordelo@souunit.com.br>
2026-01-20 15:21:19 -0300

    add: Adição do componente de Input

---
+++ easy-pay/src/components/InputTexto.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+import { computed, ref, type Ref } from 'vue';
+import {validar, type campoPossivel} from "../utils/validacoes"
+    const txtDigitado = ref("")
+
+    const props = defineProps<{
+        placeholder: string
+        label?: string 
+        type: string
+        modelValue : string
+        validacao ?: campoPossivel
+    }>()
+
+    const emit = defineEmits<{
+        (e: 'update:modelValue', value:string) :void
+    }>()
+
+    const tipo = ref(props.type);
+
+    const esconder = props.type==="password" ? true : false;
+    function revelarSenha(){
+        tipo.value = "text"
+    }
+
+    function esconderSenha(){
+        tipo.value = "password"
+    }
+
+    const textoErro = ref("")
+
+    function validarString(){
+        const resultado = validar(txtDigitado.value,props.validacao)
+        if (resultado != undefined){
+            textoErro.value = resultado;
+        }
+         
+    }
+
+
+</script>
+
+<template>
+    <div>
+        <label for="texto" v-if="label" class="labelInput">{{ label }}</label>
+        <div class="digitar">
+            <input  :type="tipo" id="texto" :placeholder="placeholder" 
+            v-model="txtDigitado" @input="emit('update:modelValue', txtDigitado)" @blur="validarString()">
+            <button type="button" v-if="esconder" @mousedown="revelarSenha" @mouseup="esconderSenha"></button>
+        </div>
+        <span v-if="textoErro" class="textoDeErro">{{textoErro}}</span>
+    </div>
+</template>
+
+<style scoped>
+    div{
+        display: flex;
+        flex-direction: column;
+    }
+
+    input{
+        background-color: unset;
+        border: 0;
+        width: 100%;
+    }
+
+    input:focus{
+        outline: none;
+        box-shadow: none;
+    }
+
+    .labelInput{
+        color: var(--cinza-escuro);
+        font-weight: 600;
+        font-size: 15px;
+    }
+
+    .digitar{
+        border: 0;
+        background-color: var(--cinza-mais-claro);
+        border-radius: 2px;
+        padding: 15px;
+        display: flex;
+        flex-direction: row;
+    }
+
+    .textoDeErro{
+        margin: 0;
+        color: red;
+        font-size: 12px;
+        margin-top: -10px;
+    }
+
+    
+</style>
