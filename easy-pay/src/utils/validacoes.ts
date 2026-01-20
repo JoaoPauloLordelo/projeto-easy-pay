e7cf97060fd7d812e06b82b5f9e28595988b1878
João Paulo <joao.lordelo@souunit.com.br>
2026-01-20 19:13:38 -0300

    fix: Conserto das validações

--- easy-pay/src/utils/validacoes.ts
+++ easy-pay/src/utils/validacoes.ts
@@ -1,30 +1,20 @@
 import type { Ref } from 'vue';    
     
-export type campoPossivel = "senha" | "cpf" | "email" | "data" | ""
+export type campoPossivel = "senha" | "cpf" | "email" | "data"
     
 const validacoes = {
-        "senha": (valor : string) => {
-            return validarSenha(valor)
-        },
-        "cpf": (valor : string) =>{
-            return validarCpf(valor)
-        },
-        "email": (valor : string)=>{
-            return validarEmail(valor)
-        },
-        "data": (valor : string)=>{
-            return ""
-        },
-        "":()=>{
-            return ""
-        }
+        "senha": validarSenha,
+        "cpf":   validarCpf,
+        "email": validarEmail,
+        "data":  validarData
+       ,
+
     }
 
 export function validar(valor : string, tipo ?: campoPossivel){
     if (tipo != undefined){
         return validacoes[tipo](valor);
     }
-
 }
 
 
@@ -33,12 +23,14 @@ function validarSenha(valor: string){
     if (valor.length < 8){
         return "AVISO: A senha possui menos que 8 digitos"
     }
+    return ""
 }
 
 function validarCpf(valor: string){
     if(valor.length < 11){
         return "AVISO: Seu cpf está incompleto!"
     }
+    return ""
     //Implementar o algoritimo do cpf
 }
 
@@ -46,8 +38,27 @@ function validarEmail(valor: string){
     if (!valor.includes("@")){
         return "AVISO: Seu email está fora do padrão"
     }
+    return ""
 }
 
 function validarData(valor: string){
+    const dataAtual = new Date()
+    const dataNascimento = new Date(valor);
+
+    if (dataAtual.getFullYear() - dataNascimento.getFullYear() < 18){
+            window.alert("1")
+        
+            return "AVISO: Você está abaixo da idade mínima, 18 anos"
+    }
+  if (
+    dataAtual.getMonth() < dataNascimento.getMonth() ||
+    (
+      dataAtual.getMonth() === dataNascimento.getMonth() &&
+      dataAtual.getDate() < dataNascimento.getDate() +1
+    )) {
+    return "AVISO: Você está abaixo da idade mínima, 18 anos"
+  }
+
+    return ""
 
 }
