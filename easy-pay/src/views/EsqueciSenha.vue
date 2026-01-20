10ad50462c79bdca7e4333fa0ec15da0d35753e5
Joao Paulo Lordelo <joao.lordelo@souunit.com.br>
2026-01-20 15:20:19 -0300

    add: Adição da pagina de Esqueci a Senha

---
+++ easy-pay/src/views/EsqueciSenha.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import FormLogin from '@/components/FormLogin.vue';
+import FormResetSenha from '@/components/FormResetSenha.vue';
+import { ref } from 'vue';
+</script>
+
+    
+
+<template>
+    <main class="fundoTela">
+        <FormResetSenha/>
+    </main>
+</template>
+
+<style scoped>
+    .fundoTela{
+        background-color: var(--laranja);
+        overflow-y: hidden;
+    }
+
+</style>
