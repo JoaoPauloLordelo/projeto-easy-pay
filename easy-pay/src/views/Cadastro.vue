19d01d25c04398ee5addfb2dd559df535172f042
Joao Paulo Lordelo <joao.lordelo@souunit.com.br>
2026-01-20 15:20:37 -0300

    add: Adicao da pagina de cadastro

---
+++ easy-pay/src/views/Cadastro.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import FormCadastro from '@/components/FormCadastro.vue';
+import { ref } from 'vue';
+</script>
+
+    
+
+<template>
+    <main class="fundoTela">
+        <FormCadastro/>
+    </main>
+</template>
+
+<style scoped>
+    .fundoTela{
+        background-color: var(--laranja);
+        overflow-y: hidden;
+    }
+
+</style>
