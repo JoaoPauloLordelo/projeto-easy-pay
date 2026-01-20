7a41846d538d84f6f8529d9f851b3bfd7d6945f9
Joao Paulo Lordelo <joao.lordelo@souunit.com.br>
2026-01-20 15:19:46 -0300

    add: ADição da pagina de login

---
+++ easy-pay/src/views/Login.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import FormLogin from '@/components/FormLogin.vue';
+import { ref } from 'vue';
+</script>
+
+    
+
+<template>
+    <main class="fundoTela">
+        <FormLogin/>
+    </main>
+</template>
+
+<style scoped>
+    .fundoTela{
+        background-color: var(--laranja);
+        overflow-y: hidden;
+    }
+
+</style>
