b5b2f58b7452e06a390fba9144da1633671f635d
Joao Paulo Lordelo <joao.lordelo@souunit.com.br>
2026-01-20 15:22:51 -0300

    add: Componente para Botao de tamanho medio

---
+++ easy-pay/src/components/BotaoMedio.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type { ButtonHTMLAttributes } from 'vue';
+
+
+    const props = defineProps<{
+        name : string
+        type?: "button" | "submit" | "reset"
+    }>()
+
+</script>
+
+<template>
+    <button :type="type" class="botaoMedio">
+        {{ name }}
+    </button>
+</template>
+
+<style scoped>
+    .botaoMedio{
+        border: 0;
+        padding: 10px;
+        color : var(--cor-de-fundo);
+        background-color: var(--laranja);
+        border-radius: 5px;
+        cursor: pointer;
+        transition: 0.2s;
+        font-weight: 600;
+    }
+
+    .botaoMedio:hover{
+        background-color: var(--laranja-escuro);
+        color: var(--cinza);
+    }
+</style>
