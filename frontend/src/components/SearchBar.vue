b82a9f985fa391f463287e92f77b1a82569d5e7f
Joao Paulo Lordelo <joao.lordelo@souunit.com.br>
2026-02-04 12:34:38 -0300

    add: Criação do componente de barra de pesquisa

---
+++ frontend/src/components/SearchBar.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import InputTexto from './InputTexto.vue';
+
+    const pesquisar = ref("")
+
+</script>
+
+<template>
+    <InputTexto placeholder="Pesquisar Pagamento" type="text" v-model="pesquisar" :txt-digitado="pesquisar"/>
+</template>
+
+
+<style scoped></style>
